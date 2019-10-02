18570eae5318629992a8beeb0bdc6ad53b38a6df
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-10-02 15:18:28 -0400

    update page name and add font

--- src/WelcomeOverlay.js
+++ src/WelcomeOverlay.js
@@ -3,7 +3,7 @@ import React from "react";
 const WelcomeOverlay = ({ onClose }) => {
   return (
     <div className="overlay">
-      <h1>Welcome</h1>
+      <h1>Baby in Tow</h1>
       <div></div>
       <button onClick={onClose}>Let&apos;s go!</button>
     </div>
