ef618d69aa7e786f0bdb4e58fe9a5689f17f6d70
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-10-28 12:25:40 -0400

    improve style of filter view

--- src/FiltersOverlay.js
+++ src/FiltersOverlay.js
@@ -1,15 +1,22 @@
 import React from "react";
-import closeIcon from "./icons/close.png";
+import closeIcon from "./icons/close.svg";
 
 const FiltersOverlay = ({ filters, updateFilters, onClose }) => {
   return (
-    <div className="overlay">
+    <div
+      className="overlay"
+      style={{
+        color: "#374B5B"
+      }}
+    >
       <button
-        className="controlButton"
         style={{
           position: "absolute",
           top: 0,
           right: 0,
+          backgroundColor: "transparent",
+          border: "none",
+          cursor: "pointer",
           margin: "10px",
           padding: "5px"
         }}
@@ -17,87 +24,106 @@ const FiltersOverlay = ({ filters, updateFilters, onClose }) => {
       >
         <img src={closeIcon} alt="Close"></img>
       </button>
-      <h1>Filters</h1>
-      <h2>Type of Place</h2>
-      <div>
-        <input
-          type="checkbox"
-          name="indoor"
-          checked={filters.indoor}
-          onChange={e => {
-            const checked = e.target.checked;
-            updateFilters(filters => ({ ...filters, indoor: checked }));
-          }}
-        />
-        <label htmlFor="indoor">Indoor</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          name="outdoor"
-          checked={filters.outdoor}
-          onChange={e => {
-            const checked = e.target.checked;
-            updateFilters(filters => ({
-              ...filters,
-              outdoor: checked
-            }));
-          }}
-        />
-        <label htmlFor="outdoor">Outdoor</label>
-      </div>
 
-      <h2>Stroller Friendly?</h2>
-      <select
-        value={filters.stroller}
-        onChange={e => {
-          const value = e.target.value;
-          updateFilters(filters => ({ ...filters, stroller: value }));
-        }}
-        onBlur={e => {
-          const value = e.target.value;
-          updateFilters(filters => ({ ...filters, stroller: value }));
+      <div
+        style={{
+          fontSize: "24px",
+          fontWeight: "bold"
         }}
       >
-        <option value="">Any</option>
-        <option value="1">Doable with a bit of effort</option>
-        <option value="2">Moderate space</option>
-        <option value="3">It&apos;s a stroller party!</option>
-      </select>
+        Filters
+      </div>
 
-      <h2>Whip &apos;em out?</h2>
-      <select
-        value={filters.nursing}
-        onChange={e => {
-          const value = e.target.value;
-          updateFilters(filters => ({ ...filters, nursing: value }));
-        }}
-        onBlur={e => {
-          const value = e.target.value;
-          updateFilters(filters => ({ ...filters, nursing: value }));
-        }}
-      >
-        <option value="">Any</option>
-        <option value="1">Public, but chill vibe</option>
-        <option value="2">Some privacy</option>
-        <option value="3">Dedicated space</option>
-      </select>
+      <div className="filterSection">
+        <div className="filterLabel">Type of Place</div>
+        <div>
+          <input
+            type="checkbox"
+            name="indoor"
+            checked={filters.indoor}
+            onChange={e => {
+              const checked = e.target.checked;
+              updateFilters(filters => ({ ...filters, indoor: checked }));
+            }}
+          />
+          <label htmlFor="indoor">Indoor</label>
+          <input
+            type="checkbox"
+            style={{
+              marginLeft: "24px"
+            }}
+            name="outdoor"
+            checked={filters.outdoor}
+            onChange={e => {
+              const checked = e.target.checked;
+              updateFilters(filters => ({
+                ...filters,
+                outdoor: checked
+              }));
+            }}
+          />
+          <label htmlFor="outdoor">Outdoor</label>
+        </div>
+      </div>
 
-      <h2>Blowout emergency?</h2>
-      <div>
-        <input
-          type="checkbox"
-          name="changeTable"
-          checked={filters.changeTable}
+      <div className="filterSection">
+        <div className="filterLabel">Change table availability</div>
+        <div>
+          <input
+            type="checkbox"
+            name="changeTable"
+            checked={filters.changeTable}
+            onChange={e => {
+              const checked = e.target.checked;
+              updateFilters(filters => ({
+                ...filters,
+                changeTable: checked
+              }));
+            }}
+          />
+          <label htmlFor="changeTable">Must have a change table</label>
+        </div>
+      </div>
+
+      <div className="filterSection">
+        <div className="filterLabel">Stroller space</div>
+
+        <select
+          value={filters.stroller}
           onChange={e => {
-            const checked = e.target.checked;
-            updateFilters(filters => ({
-              ...filters,
-              changeTable: checked
-            }));
+            const value = e.target.value;
+            updateFilters(filters => ({ ...filters, stroller: value }));
           }}
-        />
-        <label htmlFor="changeTable">Must have a change table</label>
+          onBlur={e => {
+            const value = e.target.value;
+            updateFilters(filters => ({ ...filters, stroller: value }));
+          }}
+        >
+          <option value="">Any</option>
+          <option value="1">Doable with a bit of effort</option>
+          <option value="2">Moderate space</option>
+          <option value="3">It&apos;s a stroller party!</option>
+        </select>
+      </div>
+
+      <div className="filterSection">
+        <div className="filterLabel">Ease of nursing/pumping</div>
+        <select
+          value={filters.nursing}
+          onChange={e => {
+            const value = e.target.value;
+            updateFilters(filters => ({ ...filters, nursing: value }));
+          }}
+          onBlur={e => {
+            const value = e.target.value;
+            updateFilters(filters => ({ ...filters, nursing: value }));
+          }}
+        >
+          <option value="">Any</option>
+          <option value="1">Public, but chill vibe</option>
+          <option value="2">Some privacy</option>
+          <option value="3">Dedicated space</option>
+        </select>
       </div>
     </div>
   );
