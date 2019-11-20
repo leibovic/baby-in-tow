e8dba505a7541276e6b31e7725476913cce217f7
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-11-20 09:42:00 -0500

    Create submitTips lambda

---
+++ src/lambda/submitTips.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const { google } = require("googleapis");
+
+module.exports.handler = async (event, context) => {
+  console.log(event);
+
+  const jwtClient = new google.auth.JWT(
+    process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+    null,
+    process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+    ["https://www.googleapis.com/auth/spreadsheets"]
+  );
+  const result = await jwtClient.authorize().catch(e => console.error(e));
+  console.log(result);
+
+  const sheets = google.sheets({
+    version: "v4",
+    auth: jwtClient
+  });
+
+  const sheetsResponse = await sheets.spreadsheets.values.get({
+    spreadsheetId: "1FQJHr9FR8Qgprzh8ggxQs4_InP4bZIJhEwDgAlbcOAQ",
+    range: "MVP Data!A2:Q100"
+  });
+  // console.log(sheetsResponse.data.values);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(sheetsResponse.data.values)
+  };
+
+  return response;
+};
