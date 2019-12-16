a3dd2d3a4f885a5300ae38879cca4b7f7fec7836
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-12-16 14:47:15 -0500

    workaround for multiline env variables

--- src/lambda/submitTips.js
+++ src/lambda/submitTips.js
@@ -4,10 +4,13 @@ const { google } = require("googleapis");
 module.exports.handler = async (event, context) => {
   console.log(event);
 
+  // Workaround to have a single line environment variable in Netlify
+  const key = process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/_/g, "\n");
+
   const jwtClient = new google.auth.JWT(
     process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
     null,
-    process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+    key,
     ["https://www.googleapis.com/auth/spreadsheets"]
   );
   const result = await jwtClient.authorize().catch(e => console.error(e));
