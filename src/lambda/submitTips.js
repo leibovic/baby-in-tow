dfba9fe7ace4a2e19e62698fa02d3381b9a56496
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-12-16 21:08:48 -0500

    write to spreadsheet from API

--- src/lambda/submitTips.js
+++ src/lambda/submitTips.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const { google } = require("googleapis");
 
-module.exports.handler = async (event, context) => {
-  console.log(event);
+module.exports.handler = async event => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
 
   // Workaround to have a single line environment variable in Netlify
   const key = process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/_/g, "\n");
@@ -13,23 +16,26 @@ module.exports.handler = async (event, context) => {
     key,
     ["https://www.googleapis.com/auth/spreadsheets"]
   );
-  const result = await jwtClient.authorize().catch(e => console.error(e));
-  console.log(result);
+  await jwtClient.authorize().catch(e => console.error(e));
 
   const sheets = google.sheets({
     version: "v4",
     auth: jwtClient
   });
 
-  const sheetsResponse = await sheets.spreadsheets.values.get({
+  const params = JSON.parse(event.body);
+  const res = await sheets.spreadsheets.values.append({
     spreadsheetId: "1EdnXNPE2CwvIorGFOlB8xvNs8KDEs_FcgFgy7Xm7V9U",
-    range: "Submitted Tips!A1:C100"
+    range: "Submitted Tips!A1:C100",
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [[params.name, params.tip]]
+    }
   });
-  // console.log(sheetsResponse.data.values);
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(sheetsResponse.data.values)
+    body: JSON.stringify(res.data)
   };
 
   return response;
